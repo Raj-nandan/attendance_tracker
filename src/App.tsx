d53eff4d3fcf1956ded969f32c4f147363376e0c
Raj-nandan <nandanr532@gmail.com>
2024-11-23 15:28:09 +0530

    made changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@
 import Home from './pages/Home';
 import Dashboard from './components/Dashboard';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import SignUp from './components/SignUp';
+import Login from './components/Login';
 function App() {
   return (
     // <>
@@ -14,6 +16,8 @@ function App() {
     <Routes>
       <Route path= "/" element = {<Home />}/>
       <Route path = "/dashboard" element = { <Dashboard />}/>
+      <Route path = "/signup" element = { <SignUp />}/>
+      <Route path = "/login" element = { <Login />}/>
       
     </Routes>
     </BrowserRouter>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,29 @@
-import Login from "../components/Login"
-import SignUp from "../components/SignUp"
-
+// import Login from "../components/Login"
+import { Link } from "react-router-dom"; // Import Link for navigation
+// import SignUp from "../components/SignUp";
 
 const Home = () => {
   return (
-    <SignUp />
-    // <Login />
-  )
+    <div className="flex items-center justify-center min-h-screen bg-cover bg-center" style={{ backgroundImage: 'url("https://thumbs.dreamstime.com/b/time-attendance-tracking-system-abstract-concept-vector-illustration-clock-interactive-management-app-employee-monitoring-266952955.jpg")' }}>
+
+      <div className="bg-white bg-opacity-30 backdrop-blur-md p-6 rounded-lg shadow-lg flex space-x-4"> {/* Glass effect div */}
+
+        <Link to="/login">
+
+          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Login</button>
+        </Link>
+        <Link to="/signup">
+          <button className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">Sign Up</button>
+
+        </Link>
+
+      </div>
+
+    </div>
+
+  );
+
 }
 
-export default Home
+
+export default Home;
